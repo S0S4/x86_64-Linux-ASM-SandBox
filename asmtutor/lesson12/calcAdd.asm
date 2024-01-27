5cd47745ab255e0620981d696e2adb63276ced01
S0S4 <joselilloj9@gmail.com>
2024-01-27 23:03:31 +0100

    [SOLVED] ADD OPERATION

---
+++ asmtutor/lesson12/calcAdd.asm
@@ -0,0 +1,19 @@
+%include 'functions.asm'
+
+global _start
+
+SECTION .text
+
+_start:
+
+mov eax, 90
+mov ebx, 9
+add eax, ebx
+
+mov ecx, 0 ; Initialize counter
+
+call encontrarUnidades
+
+mov ebx, 0
+mov eax, 1
+int 80h
